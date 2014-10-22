fadb521a5ab7b7f6d708e7890a84df90e6390c4f
RoanFourie <roanfourie@gmail.com>
2014-10-22 13:30:53 +0200

    'test123'

---
+++ aaCSV.py
@@ -0,0 +1,78 @@
+"""
+Created on 21 Oct 2014
+
+Modules for use with Archestra, various things to make the job easier.
+
+USAGE:
+    %prog in_file.txt out_file.txt %keyword
+@author: Roan Fourie
+@mail: roanfourie@gmail.com
+"""
+import sys
+
+def get_values(in_file, out_file, keyword1):
+    """
+    Created on 21 Oct 2014
+    Created for use with Archestra, using the exported csv file from an object.
+    Lets say you exported a composite object to a csv file and you want all the short descriptions or all the tagnames, this function will get it for you. 
+    
+    USAGE:
+        %prog in_file.csv out_file.txt %keyword
+    @author: Roan Fourie
+    @mail: roanfourie@gmail.com
+    """
+    return_data = 0
+    str_data = ' '
+    write_data = ''
+    kw_position = -1
+    next_row = 0
+    occurences = 0
+    start_position = 0
+    count1 = 0
+    count2 = 0
+    i = 0
+    k = 0
+    try:
+        with open(in_file, 'rt') as fi:
+            for line in fi: #For each line do the following
+                str_data = line
+                k = k+1
+                if next_row == 1: #Means that a valid keyword was found in the previous row
+                    i = 0
+                    count1 = 0
+                    count2 = 0
+                    while i < len(str_data): #Iterate until the amount of commas is reached, we know the amount of commas from the previous row
+                        i = i+1
+                        if str_data[i-1] == ",": #Looks for the commas and count them with their positions in the string
+                            count1 = count1 + 1
+                            count2 = count2 + 1
+                            if count1 == occurences: #The keyword begin position is reached, get the position in the string
+                                start_position = i-1
+                            if count2 == (occurences + 1): #The keyword end position is reached, get the position in the string
+                                end_position = i-1
+                                i = len(str_data)
+                    start_position = start_position + 1 #Else the first comma is also copied for output
+                    if kw_position <= 2: #this part for when the keyword is at the beginning without a comma in front of it
+                        start_position = start_position - 1
+                    print('Value = ', str_data[start_position:end_position])
+                    write_data = str_data[start_position:end_position] + '\n' #Build the data string with a new line appended to it
+                    with open(out_file, 'at') as fo: #Write the data string to a file
+                        fo.seek(0,2)
+                        fo.write(write_data)
+                    fo.close()
+                    next_row = 0
+                kw_position = str_data.find(keyword1) #Check the keyword position in the line, -1 if not found
+                if kw_position > -1: #If the keyword is found in the line
+                    next_row = 1 #set the next row to be processed for the keyword
+                    occurences = str_data.count(',', 0, kw_position) #Check how many comma's it is up to the keyword
+    except IOError:
+        print("Error in reading/writing file.")
+        return_data = 2
+    else:
+        print('Operation completed successfully.')
+        return_data = 1
+    finally:
+        print("done")
+    return return_data
+
+get_values(sys.argv[1], sys.argv[2], sys.argv[3])
---
+++ text_tools.py
@@ -0,0 +1,113 @@
+"""
+Created on 15 Oct 2014
+
+Modules for use with Archestra, various things to make the job easier.
+
+USAGE:
+    %prog in_file.txt out_file.txt
+@author: Roan Fourie
+@mail: roanfourie@gmail.com
+"""
+import sys
+
+def create_script_tagnames(in_file, out_file):
+    """
+    Created on 15 Oct 2014
+    Created for use with Archestra to create the if statements used in the scrAssignIO scripts.
+    It takes a text file containing a list of Field Attributes and creates a new text file with the if statements.
+    
+    USAGE:
+        %prog in_file.txt out_file.txt
+    @author: Roan Fourie
+    @mail: roanfourie@gmail.com
+    """
+    return_data = 0
+    str_data = ''
+    write_data = ''
+    #current_row = 0
+    try:
+        with open(out_file, 'at') as fo:
+            fo.seek(0,2)
+            write_data = """'Wait two execution counts to make sure script executes\nIf Me.scrAssignIO.ExecutionCnt > 2 Then\n
+'Declare variables\n
+\tDIM Datasource AS STRING;
+\tDIM AliasDB AS INDIRECT;
+"""
+            fo.write(write_data)
+            #print(write_data)
+        fo.close()
+        with open(in_file, 'rt') as fi:
+            for line in fi:
+                #str_data = fi.readline()
+                str_data = line
+                with open(out_file, 'at') as fo:
+                    fo.seek(0,2)
+                    write_data = '\tDIM '+ str_data.rstrip('\n') + '_Exist AS INTEGER;\n'
+                    fo.write(write_data)
+                    #print(write_data)
+                fo.close()
+                #print(line)
+                #print(str_data)
+        with open(out_file, 'at') as fo:
+            fo.seek(0,2)
+            write_data = """\n\n'Datasource is the input/output source you want to allocate to your field attribute
+\tDatasource = Me.PLCPath + "." + Me.Tagname;\n
+'Create the alias list that is in the Device Integration object using the PLCPath uda to make it more dynamic
+\tAliasDB.bindto(Me.PLCPath + ".AliasDataBase");
+\t'The exist variables will return an integer value when the string exists in the alias database in the DI object
+\t'A string comparison is done between the Alias DB list and the Attribute to see if the attribute exists in the Alias DB
+\t'The StringChar function looks at the ASCII value for a quote which seperates the alias tags from each other in the aliasDB\n
+"""
+            fo.write(write_data)
+            #print(write_data)
+        with open(in_file, 'rt') as fi:
+            for line in fi:
+                #str_data = fi.readline()
+                str_data = line
+                with open(out_file, 'at') as fo:
+                    fo.seek(0,2)
+                    write_data = '\t' + str_data.rstrip('\n') + '_Exist = StringInString(AliasDB, (StringChar(34) + Me.TagName + ".'+ str_data.rstrip('\n') +'" + StringChar(34)),1,0);\n'
+                    fo.write(write_data)
+                    #print(write_data)
+                fo.close()
+                #print(line)
+        with open(out_file, 'at') as fo:
+            fo.seek(0,2)
+            write_data = '\n\n'+"\t'"+ 'The value is then assigned to the DIN Field Attribute input source or unassigned if it is not in the AliasDB\n'
+            fo.write(write_data)
+            #print(write_data)
+        with open(in_file, 'rt') as fi:
+            for line in fi:
+                #str_data = fi.readline()
+                str_data = line
+                with open(out_file, 'at') as fo:
+                    fo.seek(0,2)
+                    write_data = '\tIf %s_Exist > 0 Then\n\t\tMe.%s.Input.InputSource = Datasource + ".%s";\n\tElse\n\t\tMe.%s.Input.InputSource = "---";\n\tEndIf;\n\n' % (str_data.rstrip('\n'), str_data.rstrip('\n'), str_data.rstrip('\n'), str_data.rstrip('\n'))
+                    fo.write(write_data)
+                    #print(write_data)
+                fo.close()
+                #print(line)
+        with open(out_file, 'at') as fo:
+            fo.seek(0,2)
+            write_data = """\n'The script is then finished by setting the trigger bit to true and it will stop executing
+    Me.AssignIOCmd = True;\n
+EndIf;\n
+'A Log Message flag is added to see if the script executed in the SMC - This can be excluded and is for troubleshooting
+'LogMessage (Me.Tagname + "Finished");\n
+"""
+            fo.write(write_data)
+            #print(write_data)
+        fo.close()
+    except IOError:
+        print("Error in reading/writing file.")
+        return_data = 2
+    else:
+        print('Operation completed successfully.')
+        return_data = 1
+    finally:
+        fi.close()
+        #fo.close()
+        print("done")
+    return return_data
+
+create_script_tagnames(sys.argv[1], sys.argv[2])
